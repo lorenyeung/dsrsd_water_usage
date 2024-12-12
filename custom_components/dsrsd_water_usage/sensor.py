343631bddea332ae6d7c805e9953ef0b8b1fb0a2
Loren Yeung <loren.w.yeung@gmail.com>
2024-12-11 23:27:58 -0800

    fix inputs 2

--- custom_components/dsrsd_water_usage/sensor.py
+++ custom_components/dsrsd_water_usage/sensor.py
@@ -144,7 +144,7 @@ class DSRSDWaterUsage(Entity):
     async def async_update(self):
         try:
             _LOGGER.debug("Getting Time Series Data")
-            new_data = await self.hass.async_add_executor_job(self.get_water_usage, 7)  # Fetch data for 7 days
+            #new_data = await self.hass.async_add_executor_job(self.get_water_usage, 7)  # Fetch data for 7 days
             _LOGGER.debug("Getting Time Series Data failed get maybe: %s", new_data)
             if False:
                 self.update_statistics(new_data)
